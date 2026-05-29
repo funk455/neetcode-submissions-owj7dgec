32d7308f6701a755f42381f2ef527a3c010d2096
funk455 <109295348+funk455@users.noreply.github.com>
2026-05-29 19:40:18 +0800

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> mem(coins.size()+1,vector<int>(amount+1,-1));
+        return dfs(mem,0,amount,coins);
+    }
+    int dfs( vector<vector<int>> &mem,int i, int amount,vector<int>& coins) {
+        if (amount<0 || i >=coins.size()) return 0;
+        if (amount==0) return 1;
+        if (mem[i][amount] != -1) return mem[i][amount];
+        int exclude = dfs(mem,i+1,amount,coins);
+        int include = 0;
+        if (amount>=coins[i]){
+            include=dfs(mem,i,amount-coins[i],coins);
+        }
+        return  mem[i][amount] = exclude+include;
+    }
+};
