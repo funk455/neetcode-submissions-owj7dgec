df3aab5fc2adaeb533eb8dde0e5c1a0efcc477eb
funk455 <109295348+funk455@users.noreply.github.com>
2026-07-01 22:16:06 +0800

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            c = (n>>i) & 1
+            res+=(c<<(31-i))
+        return res
